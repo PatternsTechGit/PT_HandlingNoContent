548739aec4057466614c288f757163cef6d750cc
waqasbt <waqas-dev@outlook.com>
2022-08-02 23:13:13 +0500

    Before & Original Code

---
+++ Original/BBBank_UI/src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { LineGraphData } from './models/line-graph-data'
+import { TransactionService } from './services/transaction.service';
+import {AccountByX} from './models/account-by-x';
+import AccountsService from './services/accounts.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+
+  title = 'BBBankUI';
+  lineGraphData: LineGraphData;
+  toAccount: AccountByX;
+  constructor(private transactionService: TransactionService,private accountsService: AccountsService) { }
+
+  ngOnInit(): void {
+    this.initializeTo();
+    this.transactionService
+      .getLast12MonthBalances('')
+      .subscribe({
+        next: (data) => {
+          this.lineGraphData = data.result;
+        },
+        error: (error) => {
+          console.log(error.responseException.exceptionMessage);
+        },
+      });
+  }
+
+  getToAccount() {
+    this.accountsService
+      .getAccountByAccountNumber(this.toAccount.accountNumber)
+      .subscribe({
+        next: (data) => {
+          if (data.statusCode == 204) {
+            this.initializeTo();
+          }
+          else {
+            this.toAccount = data.result
+          }
+        },
+        error: (error) => {
+        },
+      });
+  }
+
+  initializeTo() {
+    this.toAccount = new AccountByX();
+    this.toAccount.userImageUrl = '../../../assets/images/No-Image.png'
+  }
+}
